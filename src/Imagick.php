778ee91af01cf2b23e4fbc4d538644935ae65bf2
Compolomus <compolom@gmail.com>
2018-03-21 04:23:02 +0200

    Test new feature Write copyright text

--- src/Imagick.php
+++ src/Imagick.php
@@ -103,6 +103,85 @@ class Imagick extends AbstractImage implements ImageInterface
         return $this;
     }
 
+    public function getFontsList(): array
+    {
+        return $this->getImage()->queryFonts();
+    }
+
+    /**
+     * @param string $text
+     * @param string $position
+     * @param string $font
+     * @return $this
+     * @throws \Exception
+     */
+    public function copyright(string $text, string $position = 'SouthWest', string $font = 'Courier')
+    {
+        $positions = [
+            'NORTHWEST' => \Imagick::GRAVITY_NORTHWEST,
+            'NORTH' => \Imagick::GRAVITY_NORTH,
+            'NORTHEAST' => \Imagick::GRAVITY_NORTHEAST,
+            'WEST' => \Imagick::GRAVITY_WEST,
+            'CENTER' => \Imagick::GRAVITY_CENTER,
+            'SOUTHWEST' => \Imagick::GRAVITY_SOUTHWEST,
+            'SOUTH' => \Imagick::GRAVITY_SOUTH,
+            'SOUTHEAST' => \Imagick::GRAVITY_SOUTHEAST,
+            'EAST' => \Imagick::GRAVITY_EAST
+        ];
+
+        if (!in_array($font, $this->getFontsList())) {
+            throw new \Exception('Does not support font');
+        }
+        if (!array_key_exists(strtoupper($position), $positions)) {
+            throw new \Exception('Wrong position');
+        }
+
+        $position = $positions[strtoupper($position)];
+
+        $image = $this->prepareImage($text, $position, $font);
+        $this->getImage()->compositeImage($image, \Imagick::COMPOSITE_DISSOLVE, 0, 0);
+
+        return $this;
+    }
+
+
+    /**
+     * @param string $text
+     * @param int $position
+     * @param string $font
+     * @return \Imagick
+     */
+    private function prepareImage(string $text, int $position, string $font)
+    {
+        $image = new \Imagick();
+        $mask = new \Imagick();
+        $draw = new \ImagickDraw();
+
+        $width = $this->getWidth();
+        $height = $this->getHeight();
+
+        $image->newImage($width, $height, new \ImagickPixel('grey30'));
+        $mask->newImage($width, $height, new \ImagickPixel('black'));
+
+        $draw->setFont($font);
+        $draw->setFontSize(20);
+        $draw->setFillColor('grey70');
+
+        $draw->setGravity($position);
+
+        $image->annotateImage($draw, 10, 12, 0, $text);
+        $draw->setFillColor('white');
+        $mask->annotateImage($draw, 11, 13, 0, $text);
+        $mask->annotateImage($draw, 10, 12, 0, $text);
+        $draw->setFillColor('black');
+        $mask->annotateImage($draw, 9, 11, 0, $text);
+        $mask->setImageMatte(false);
+
+        $image->compositeImage($mask, \Imagick::COMPOSITE_COPYOPACITY, 0, 0);
+
+        return $image;
+    }
+
     public function crop(int $width, int $height, int $startX, int $startY): ImageInterface
     {
         $this->getImage()->cropImage($width, $height, $startX, $startY);

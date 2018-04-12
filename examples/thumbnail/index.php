e1c436302909c5025743cd63345872e7a6a209ef
Compolomus <compolom@gmail.com>
2018-04-12 20:56:05 +0200

    Add check loaded extension Imagick in examples

--- examples/thumbnail/index.php
+++ examples/thumbnail/index.php
@@ -4,15 +4,12 @@ use Compolomus\Compomage\Image;
 
 require '../../vendor/autoload.php';
 
+// test GD
+
 (new Image('../crop/bee.jpg', Image::GD))
     ->copyright('Test', realpath('../arial.ttf'), 'CENTER')
     ->thumbnail(170, 180)
     ->save('test1');
-
-(new Image(base64_encode(file_get_contents('../test.jpg')), Image::IMAGICK))
-    ->copyright('Test', 'Courier', 'CENTER')
-    ->thumbnail(170, 180)
-    ->save('test2');
 
 $URL_image = 'https://4.bp.blogspot.com/-P_yzboTrLUM/WGP4FUvVAQI/AAAAAAAABGc/SkRu_mOPKOwxsxLic-dBhugEyvPgvLEqgCLcB/s320/1.png';
 
@@ -21,52 +18,43 @@ $URL_image = 'https://4.bp.blogspot.com/-P_yzboTrLUM/WGP4FUvVAQI/AAAAAAAABGc/SkR
     ->thumbnail(170, 180)
     ->save('test3');
 
-// test GD
-
 $base64_image = base64_encode(file_get_contents('../test.jpg'));
 
 $img = new Image($base64_image, Image::GD);
-
 $img->thumbnail(200, 100);
-
 echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
-
 echo '<pre>' . print_r($img, true) . '</pre>';
-
 $img->save('thumbnail_test1_gd');
 
 $base64_image1 = base64_encode(file_get_contents('../crop/bee.jpg'));
 
 $img = new Image($base64_image1, Image::GD);
-
 $img->thumbnail(170, 180);
-
 echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
-
 echo '<pre>' . print_r($img, true) . '</pre>';
-
 $img->save('thumbnail_test2_gd');
+
+if (extension_loaded('imagick')) {
 
 // test Imagick
 
-$img = new Image($base64_image, Image::IMAGICK);
-
-$img->thumbnail(200, 100);
-
-echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
-
-echo '<pre>' . print_r($img, true) . '</pre>';
-
-$img->save('thumbnail_test1_im');
-
-$img = new Image($base64_image1, Image::IMAGICK);
-
-$img->thumbnail(170, 180);
-
-echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
-
-echo '<pre>' . print_r($img, true) . '</pre>';
-
-// save Imagick test
-
-$img->save('thumbnail_test2_im');
+	(new Image(base64_encode(file_get_contents('../test.jpg')), Image::IMAGICK))
+		->copyright('Test', 'Courier', 'CENTER')
+		->thumbnail(170, 180)
+		->save('test2');
+		
+	$img = new Image($base64_image, Image::IMAGICK);
+	$img->thumbnail(200, 100);
+	echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
+	echo '<pre>' . print_r($img, true) . '</pre>';
+	$img->save('thumbnail_test1_im');
+	
+	$img = new Image($base64_image1, Image::IMAGICK);
+	$img->thumbnail(170, 180);
+	echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
+	echo '<pre>' . print_r($img, true) . '</pre>';
+	// save Imagick test
+	$img->save('thumbnail_test2_im');
+} else {
+	echo 'Imagick not supported'
+}
--- examples/watermark/index.php
+++ examples/watermark/index.php
@@ -9,29 +9,22 @@ require '../../vendor/autoload.php';
 $base64_image = base64_encode(file_get_contents('../test.jpg'));
 
 $img = new Image($base64_image, Image::GD);
-
 $watermark = new Image('ImageMagick.png', Image::GD);
-
 echo '<img src="data:image/png;base64,' . $watermark->resizeByHeight(100)->getBase64() . '" alt="base64_image" style="background-color: white;" />';
-
 $watermark->resizeByHeight(100);
-
 $img->watermark($watermark, 'CENTER');
-
 $img->copyright('GD test', realpath('../couri.ttf'), 'SOUTHEAST');
-
-
 echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
 
+if (extension_loaded('imagick')) {
 // test Imagick
 
-$img = new Image($base64_image, Image::IMAGICK);
-
-$watermark = new Image('ImageMagick.png', Image::IMAGICK);
-$watermark->resizeByHeight(100);
-
-$img->watermark($watermark, 'CENTER');
-
-$img->copyright('Imagick test', 'Courier', 'SOUTHEAST');
-
-echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
+	$img = new Image($base64_image, Image::IMAGICK);
+	$watermark = new Image('ImageMagick.png', Image::IMAGICK);
+	$watermark->resizeByHeight(100);
+	$img->watermark($watermark, 'CENTER');
+	$img->copyright('Imagick test', 'Courier', 'SOUTHEAST');
+	echo '<img src="data:image/png;base64,' . $img->getBase64() . '" alt="base64_image" style="background-color: orange;" />';
+} else {
+	echo 'Imagick not supported'
+}

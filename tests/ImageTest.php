b31adee43ced889acafb4f1037a4a42fa4389f5a
Compolomus <compolom@gmail.com>
2022-07-17 15:14:17 +0200

    Fix tests

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -9,13 +9,13 @@ use Imagick;
 
 class ImageTest extends TestCase
 {
-    public static Image $imageGD;
-    public static Image $imageImagick;
-
-    protected function setUp(): void
+    public Image $imageGD;
+    public Image $imageImagick;
+    
+    public function setUp(): void
     {
-        self::$imageGD = new Image(dirname(__FILE__, 2) . '/examples/test.jpg', Image::GD);
-        self::$imageImagick = new Image(dirname(__FILE__, 2) . '/examples/crop/bee.jpg', Image::IMAGICK);
+        $this->imageGD = new Image(dirname(__FILE__, 2) . '/examples/test.jpg', Image::GD);
+        $this->imageImagick = new Image(dirname(__FILE__, 2) . '/examples/crop/bee.jpg', Image::IMAGICK);
     }
 
     /**
@@ -70,61 +70,61 @@ class ImageTest extends TestCase
 
     public function testResizesBy(): void
     {
-        self::$imageGD->resizeBy('width', 100);
-        $this->assertEquals(100, self::$imageGD->getWidth());
-        self::$imageGD->resizeBy('height', 150);
-        $this->assertEquals(150, self::$imageGD->getHeight());
-        self::$imageGD->resizeBy('percent', 50);
-        $this->assertEquals(75, self::$imageGD->getHeight());
+        $this->imageGD->resizeBy('width', 100);
+        $this->assertEquals(100, $this->imageGD->getWidth());
+        $this->imageGD->resizeBy('height', 150);
+        $this->assertEquals(150, $this->imageGD->getHeight());
+        $this->imageGD->resizeBy('percent', 50);
+        $this->assertEquals(75, $this->imageGD->getHeight());
     }
 
     public function testThumbnailGD(): void
     {
-        self::$imageGD->thumbnail(100, 100);
-        $this->assertEquals(100, self::$imageGD->getHeight());
-        $this->assertEquals(100, self::$imageGD->getWidth());
+        $this->imageGD->thumbnail(100, 100);
+        $this->assertEquals(100, $this->imageGD->getHeight());
+        $this->assertEquals(100, $this->imageGD->getWidth());
     }
 
     public function testThumbnailImagick(): void
     {
-        self::$imageImagick->thumbnail(100, 100);
-        $this->assertEquals(100, self::$imageImagick->getHeight());
-        $this->assertEquals(100, self::$imageImagick->getWidth());
+        $this->imageImagick->thumbnail(100, 100);
+        $this->assertEquals(100, $this->imageImagick->getHeight());
+        $this->assertEquals(100, $this->imageImagick->getWidth());
     }
 
     public function testGetFontsList(): void
     {
-        $this->assertIsArray(self::$imageImagick->getFontsList());
+        $this->assertIsArray($this->imageImagick->getFontsList());
     }
 
     public function testGetBase64(): void
     {
-        $gdBase64 = self::$imageGD->getBase64();
+        $gdBase64 = $this->imageGD->getBase64();
         $obj = new Image($gdBase64, Image::GD);
 
-        $imagickBase64 = self::$imageImagick->getBase64();
+        $imagickBase64 = $this->imageImagick->getBase64();
         $obj = new Image($imagickBase64, Image::IMAGICK);
         $this->assertInstanceOf(Imagick::class, $obj->getImage());
     }
 
     public function testResize(): void
     {
-        self::$imageGD->resize(170, 150);
-        $this->assertEquals(170, self::$imageGD->getWidth());
-        $this->assertEquals(150, self::$imageGD->getHeight());
+        $this->imageGD->resize(170, 150);
+        $this->assertEquals(170, $this->imageGD->getWidth());
+        $this->assertEquals(150, $this->imageGD->getHeight());
 
-        self::$imageImagick->resize(160, 140);
-        $this->assertEquals(160, self::$imageImagick->getWidth());
-        $this->assertEquals(140, self::$imageImagick->getHeight());
+        $this->imageImagick->resize(160, 140);
+        $this->assertEquals(160, $this->imageImagick->getWidth());
+        $this->assertEquals(140, $this->imageImagick->getHeight());
     }
 
     public function testRotate(): void
     {
-        self::$imageGD->rotate();
-        $this->assertEquals(609, self::$imageGD->getWidth());
+        $this->imageGD->rotate();
+        $this->assertEquals(609, $this->imageGD->getWidth());
 
-        self::$imageImagick->rotate();
-        $this->assertEquals(300, self::$imageImagick->getWidth());
+        $this->imageImagick->rotate();
+        $this->assertEquals(300, $this->imageImagick->getWidth());
     }
 
     public function testSave(): void
@@ -135,10 +135,10 @@ class ImageTest extends TestCase
         mkdir($gdFile, 0777, true);
         mkdir($imagickFile, 0777, true);
 
-        self::$imageGD->save($gdFile);
+        $this->imageGD->save($gdFile);
         $this->assertFileExists($gdFile . '.png');
 
-        self::$imageImagick->save($imagickFile);
+        $this->imageImagick->save($imagickFile);
         $this->assertFileExists($imagickFile . '.png');
 
         unlink($gdFile . '.png');
